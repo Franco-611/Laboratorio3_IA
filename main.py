15dc1233f2808bd7799260540c88c1eb1559c6cf
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-02-10 12:33:43 -0600

    Modelo
    Co-authored-by: Manuel-Archila <Manuel-Archila@users.noreply.github.com>
    Co-authored-by: JuanDiegoAvila <JuanDiegoAvila@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from reader import Reader 
+from modelo import Modelo
 
 reader = Reader('./entrenamiento.txt')
-print(reader.dictionary)
+modelo = Modelo(reader.training)
 
+
+for key in reader.testing:
+    for frase in reader.testing[key]:
+        print(frase + ' ' + key)
+        prueba = Modelo.naiveBayes(modelo, frase)
+        print(prueba)
+        input()
+    
